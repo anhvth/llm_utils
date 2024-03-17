a8216666076b6fe074384d5ea5b5acdd059e2dc2
anh vo <anhvth.226@gmail.com>
2024-03-17 20:04:28 +0700

    init

---
+++ llm_data_utils/__init__.py
@@ -0,0 +1,1 @@
+from .chat_format import *
---
+++ setup.py
@@ -0,0 +1,18 @@
+# setup.py
+from setuptools import setup, find_packages
+from setuptools import setup, find_packages
+# setup.py
+
+setup(
+    name="llm_data_utils",
+    version="0.0.1",
+    author="AnhVTH",
+    author_email="anhvth.226@gmail.com",
+    description="A small package for data processing",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
