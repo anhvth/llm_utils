8ffff9032d04c0e411a8192b2d52774da4e04f42
anhvth <anhvth.226@gmail.com>
2025-02-25 14:19:01 +0700

    Add group_messages module with functions for batching messages by length

--- llm_utils/__init__.py
+++ llm_utils/__init__.py
@@ -1,6 +1,7 @@
 from .gemini import get_gemini_response
 from .chat_format import *
-from .load_chat_dataset import load_chat_dataset
+
+# from .load_chat_dataset import load_chat_dataset
 from .meta_prompt import (
     Example,
     generate_program_json,
@@ -9,9 +10,11 @@ from .meta_prompt import (
     get_prompt_template,
 )
 from .text_utils import *
-
+from .group_messages import group_messages_by_len,split_indices_by_length
 __all__ = [
-    "load_chat_dataset",
+    # "load_chat_dataset",
+    "split_indices_by_length",
+    "group_messages_by_len",
     "get_prompt_template",
     "get_langchain_openai_model",
     "generate_program_json",
