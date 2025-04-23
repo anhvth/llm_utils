19d85c1437fcb47024b7cc2b0dd8405961aac5c8
anhvth5 <anhvth.226@gmail.com>
2025-04-23 03:30:41 +0000

    fix import

--- llm_utils/__init__.py
+++ llm_utils/__init__.py
@@ -1,6 +1,18 @@
-from .chat_format import transform_messages,transform_messages_to_chatml,display_chat_messages_as_html,get_conversation_one_turn,display_diff_two_string,display_conversations,build_chatml_input,format_msgs,
+from .chat_format import (
+    transform_messages,
+    transform_messages_to_chatml,
+    display_chat_messages_as_html,
+    get_conversation_one_turn,
+    display_diff_two_string,
+    display_conversations,
+    build_chatml_input,
+    format_msgs,
+)
 from .lm import OAI_LM
-from .group_messages import     split_indices_by_length,group_messages_by_len
+from .group_messages import (
+    split_indices_by_length,
+    group_messages_by_len,
+)
 
 __all__ = [
     "transform_messages",
